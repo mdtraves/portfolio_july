c6a0fbb81cb9391cbd77c005dce516a31fc72bee
mike <mdtraves@hotmail.com>
2017-08-30 20:15:31 +0100

    removed the invert property of the site branding logo, altered in illustrator instead, cross browser checked the site, corrected margins and opacirty of the navbar

--- main.js
+++ main.js
@@ -5,10 +5,5 @@ $(document).ready(function(){
     $("#mytitlesmall").show("puff");
     
     
-    $('img, a').hover(function(){
-   $(this).css("transform", "scale(1.01, 1.01)");
-}, function(){
-   $(this).css("transform", "none");
-});
     
 });
