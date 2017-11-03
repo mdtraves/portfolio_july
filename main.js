767fdcf23c9e19235c18ab99e318e576e8c176a3
mike <mdtraves@hotmail.com>
2017-11-03 00:32:46 +0000

    overhaul of site added bio and javascript show hide.. reduced pages to 2

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     $("#mytitle").slideDown(600);
     $("#mytitlesmall").slideDown(600);
     
-    
-    
+    $(".extra").hide();
+    $(".bioContainer2").click(function(){
+        $(".extra").slideToggle(300);
+    });
 });
